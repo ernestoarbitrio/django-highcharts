e224149339a96f67c3e4c802be8a8ea88e485723
Bruno Bord <bruno@jehaisleprintemps.net>
2013-10-31 21:58:14 +0100

    bar view to return a proper dictionary

--- highcharts/views/bar.py
+++ highcharts/views/bar.py
@@ -4,8 +4,15 @@ from django.views.generic import View
 
 class HighChartsBarView(HighChartsBasicView, View):
     x_axis = {'categories': []}
+    y_axis = {}
 
     def get_data(self):
         data = super(HighChartsBarView, self).get_data()
         data['xAxis'] = self.x_axis
+        data['yAxis'] = self.y_axis
+        data['series'] = self.series
         return data
+
+    @property
+    def series(self):
+        return []
