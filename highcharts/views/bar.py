54b22cf9876dfe1dc59f8660ef6985393ddf73e2
Bruno Bord <bruno.bord@novapost.fr>
2013-11-08 18:01:41 +0100

    simplified data dictionary building

--- highcharts/views/bar.py
+++ highcharts/views/bar.py
@@ -9,8 +9,6 @@ class HighChartsBarView(HighChartsDualAxisView, View):
     def get_data(self):
         data = super(HighChartsBarView, self).get_data()
         data['xAxis']['categories'] = self.categories
-        data['yAxis'] = self.y_axis
-        data['yAxis']['title'] = {"text": self.y_axis_title}
         data['series'] = self.series
         return data
 
--- highcharts/views/line.py
+++ highcharts/views/line.py
@@ -8,8 +8,6 @@ class HighChartsLineView(HighChartsDualAxisView, View):
     def get_data(self):
         data = super(HighChartsLineView, self).get_data()
         data['xAxis']['categories'] = self.categories
-        data['yAxis'] = self.y_axis
-        data['yAxis']['title'] = {"text": self.y_axis_title}
         data['series'] = self.series
         return data
 
