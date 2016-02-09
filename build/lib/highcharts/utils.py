20e24165041b2db11c11292b5f14125a0cb4301c
Ernesto Arbitrio <ernesto.arbitrio@gmail.com>
2016-02-09 08:48:01 +0100

    setup file edited

--- build/lib/highcharts/utils.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-
-def get_static_url(subfix='highcharts'):
-    static_url = getattr(settings, 'STATIC_URL', None)
-    if static_url:
-        return static_url
-    else:  # To old django versions
-        return '%s%s/' % (getattr(settings, 'MEDIA_URL', None), subfix)
